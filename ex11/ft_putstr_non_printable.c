813dec4f8bd02aab297dcc0add17b0df5b4d38d5
Eliot Giraud <egiraud@made-f0Ar8s6.clusters.42paris.fr>
2025-01-21 19:38:51 +0100

    all

---
+++ ex11/ft_putstr_non_printable.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egiraud <egiraud@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/19 22:38:23 by egiraud           #+#    #+#             */
+/*   Updated: 2025/01/21 15:20:43 by egiraud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int		i;
+	char	*hex;
+
+	hex = "0123456789abcdef";
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 32 && str[i] <= 126)
+			ft_putchar(str[i]);
+		else
+		{
+			ft_putchar('\\');
+			ft_putchar(hex[(unsigned char)str[i] / 16]);
+			ft_putchar(hex[(unsigned char)str[i] % 16]);
+		}
+		i++;
+	}
+}
